c6f48247ece9953ba4e2a9600ba36c495cb7a39d
Tazkir-Hossain <hossainmdtazkir@gmail.com>
2025-07-03 11:10:37 +0600

    event Listener add

---
+++ js/login.js
@@ -0,0 +1,5 @@
+document.getElementById("btn-login").addEventListener("click", function (e) {
+  e.preventDefault;
+  const phoneNumber = document.getElementById("phone-number").value;
+  console.log(phoneNumber);
+});
