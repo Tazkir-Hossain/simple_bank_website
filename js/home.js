97b341cf6f7e2d57834042218203a568c6f6b42d
Tazkir-Hossain <hossainmdtazkir@gmail.com>
2025-07-05 06:44:11 +0600

    update add money functionality

--- js/home.js
+++ js/home.js
@@ -4,5 +4,14 @@ document
     event.preventDefault();
 
     const amount = document.getElementById("amount").value;
+    const amountNumber = parseFloat(amount);
     const pinNumber = document.getElementById("pin-number").value;
+
+    // wrong way to validation
+    if (pinNumber === "1234") {
+      const balance = document.getElementById("balance").innerText;
+      const balanceNumber = parseFloat(balance);
+      const newBalance = balanceNumber + amountNumber;
+      document.getElementById("balance").innerText = newBalance;
+    }
   });
